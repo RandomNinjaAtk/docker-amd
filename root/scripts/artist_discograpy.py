93ed74662a4ef7e1e2a7a718610dd89c5fbce10b
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-11-21 08:26:08 -0500

    bugfix

--- root/scripts/artist_discograpy.py
+++ root/scripts/artist_discograpy.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from deemix.api.deezer import Deezer
+from deezer import Deezer
 import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         dz = Deezer()
-        releases = dz.get_artist_discography_gw(sys.argv[1], 100)
+        releases = dz.gw.get_artist_discography_tabs(sys.argv[1], 100)
         for type in releases:
             for release in releases[type]:
                 print(release['id'])
